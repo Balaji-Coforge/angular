dd65614456dcce48b5781459f6a65fc8efdd28b3
Balaji Krishnamoorthy <balaji.krishnamoorthy@coforgetech.com>
2020-12-03 19:39:10 +0530

    Recipe model added and used. Day 5

--- course-demo-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-demo-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Recipe desc', 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Veg_Pulao_with_onion_Raitha_02.jpg'),
+    new Recipe('A test recipe', 'Recipe desc', 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Veg_Pulao_with_onion_Raitha_02.jpg')
+  ];
 
   constructor() { }
 
